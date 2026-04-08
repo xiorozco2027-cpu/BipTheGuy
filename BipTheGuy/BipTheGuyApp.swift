c01de44cec11858f1ecd4c011ff7031edfed0327
ximena <xiorozco2027@gmail.com>
2026-04-08 10:44:57 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by OROZCO, XIMENA on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BipTheGuy
+//
+//  Created by OROZCO, XIMENA on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
