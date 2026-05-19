544ddd83fe1819bf823b32fe7afdee79bbda0fe5
ximena <xiorozco2027@gmail.com>
2026-05-19 11:01:22 -0700

    bip the guy final

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,19 +6,68 @@
 //
 
 import SwiftUI
+import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
+    @State private var audioPlayer : AVAudioPlayer!
+    @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            bipImage
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(isFullSize ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    isFullSize = false
+                    withAnimation {
+                        isFullSize = true
+                    }
+                    
+                }
+            
+            Spacer()
+            
+            PhotosPicker(selection: $selectedPhoto, matching:.images, preferredItemEncoding: .automatic) {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill" )
+            }
+            .onChange(of: selectedPhoto) {
+                Task {
+                    guard let selectedImage = try? await selectedPhoto? .loadTransferable(type: Image.self) else {
+                        print("😡 ERROR: Could not get Image from loadTransformation")
+                        return
+                    }
+                    bipImage = selectedImage
+                }
+            }
         }
-        .padding()
+            .padding()
+        }
+    
+        func playSound(soundName: String) {
+            if audioPlayer != nil && audioPlayer.isPlaying { audioPlayer.stop()
+            }
+            guard let soundFile = NSDataAsset(name: soundName) else{
+                print("😡Could not read file named \(soundName)")
+                return
+            }
+            do {
+                audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer.play()
+            } catch {
+                print("😡 ERROR: \(error.localizedDescription) creating audioplayer")
+            }
+        }
     }
-}
 
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
+
